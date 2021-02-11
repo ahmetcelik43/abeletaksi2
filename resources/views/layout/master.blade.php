8a4e6869ec4ffb39a08c198e06cef44ef94b402c
ahmetcelik43 <45917904+ahmetcelik43@users.noreply.github.com>
2021-02-11 16:10:24 +0300

    Update master.blade.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ahmet Çelik</title>
+    <title>Abelitaksi | @yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <link rel="stylesheet" href="{{ URL::asset('bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ URL::asset('all.css') }}" />
