2bb031a355db6192016a9a1700fd7618018cc1e2
ahmetcelik43 <45917904+ahmetcelik43@users.noreply.github.com>
2021-02-11 04:03:01 +0300

    Update home.blade.php

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -11,19 +11,16 @@
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <!--<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="storage/img/2.jpeg" alt="First slide">
+      <img class="d-block w-100" src="/2.jpeg" alt="First slide">
     </div>
     
     <div class="carousel-item">
-      <img class="d-block w-100"src="storage/img/1.jpeg"  alt="Second slide">
+      <img class="d-block w-100" src="/1.jpeg"  alt="Second slide">
     </div>
-    <!--<div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>-->
+ 
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
